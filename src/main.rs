00a15830bfd02d26af6b9ef7141d0502b090d76c
Aayush Prajapati <aayushprajapati0807@gmail.com>
2025-03-22 23:46:16 +0530

    code structure updated

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::collections::HashMap;
+
+trait ShellCommand{
+    fn execute(&self, args: &[&str]);
+}
+
+struct Echo;
+impl ShellCommand for Echo{
+    fn execute(&self, args: &[&str]){
+        println!("{}", args.join(" "));
+    }
+}
 
 fn main() {
     // Uncomment this block to pass the first stage
-    while true{
+    let mut commands: HashMap<&str, Box<dyn ShellCommand>> = HashMap::new();
+    commands.insert("echo", Box::new(Echo));
+
+    loop{
         print!("$ ");
         io::stdout().flush().unwrap();
 
@@ -11,11 +26,25 @@ fn main() {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-       
-        match input.trim(){
-            "exit 0" => break,
-            input if input.starts_with("echo ") => println!("{}", &input[5..]),
-            _ => println!("{}: command not found", input.trim()),
+        let input = input.trim();
+
+        let mut parts = input.split_whitespace();
+        if let Some(command) = parts.next() {
+            let args: Vec<&str> = parts.collect();
+            if command == "exit"{
+                if !args.is_empty() && args[0] == "0"{
+                    break;
+                }
+                else{
+                    continue;
+                }
+            }
+            if let Some(cmd) = commands.get(command){
+                cmd.execute(&args);
+            }
+            else{
+                println!("{}: command not found", command);
+            }
         }
     }
 }
