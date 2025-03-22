54409fa8eed09f4696f691516471a40bbbeacf81
Aayush Prajapati <aayushprajapati0807@gmail.com>
2025-03-22 20:34:17 +0530

    echo cmd added

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,11 @@ fn main() {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-        
-        if input.trim() == "exit 0"{
-            break;
-        }
-        else{
-            println!("{}: command not found", input.trim());
+       
+        match input.trim(){
+            "exit 0" => break,
+            input if input.starts_with("echo ") => println!("{}", &input[5..])
+            _ => println!("{}: command not found", input.trim())
         }
     }
 }
