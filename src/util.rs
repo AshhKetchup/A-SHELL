03c981f30c62159447722274fbb26dc838e47909
Aayush Prajapati <aayushprajapati0807@gmail.com>
2025-03-25 14:17:26 +0530

    added: echo support double quotes as well

--- src/util.rs
+++ src/util.rs
@@ -2,14 +2,18 @@ pub fn parse_input(input: &str) -> Vec<String> {
     let mut result = Vec::new();
     let mut chars = input.chars().peekable();
     let mut buffer = String::new();
-    let mut in_quotes = false;
+    let mut in_single_quotes = false;
+    let mut in_double_quotes = false;
 
     while let Some(c) = chars.next() {
         match c {
-            '\'' => {
-                in_quotes = !in_quotes;
+            '\'' if !in_double_quotes => {
+                in_single_quotes = !in_single_quotes;
             }
-            ' ' if !in_quotes => {
+            '"' if !in_single_quotes => {
+                in_double_quotes = !in_double_quotes;
+            }
+            ' ' if !in_single_quotes && !in_double_quotes => {
                 if !buffer.is_empty() {
                     result.push(buffer.clone());
                     buffer.clear();
