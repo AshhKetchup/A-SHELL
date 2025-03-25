b3541f9fbec3933495cf9dad8bac98711851a96a
Aayush Prajapati <aayushprajapati0807@gmail.com>
2025-03-25 14:12:06 +0530

    fixed: handling error cmd

--- src/commands/externalcmd.rs
+++ src/commands/externalcmd.rs
@@ -2,25 +2,20 @@ use pathsearch::find_executable_in_path;
 use std::process::Command;
 use std::path::{Path, PathBuf};
 
-pub fn externalcmd(cmd: &str, args: &[&str]) {
-    let exe_path: Option<PathBuf> = find_executable_in_path(cmd)
+
+
+pub fn externalcmd(cmd: &str, args: &Vec<&str>) -> Result<(), String>{
+    let exe_path = find_executable_in_path(cmd)
         .or_else(|| Some(Path::new(cmd).to_path_buf()))
         .filter(|path| path.exists() && path.is_file());
 
-    if let Some(path) = exe_path {
-        let exe_name = path.file_name()
-            .unwrap_or_else(|| path.as_os_str()) // Get only the filename
-            .to_string_lossy();
-
-        match Command::new(&path).args(args).spawn() {
-            Ok(mut child) => {
-                if let Err(err) = child.wait() {
-                    eprintln!("Error waiting for process: {}", err);
-                }
-            }
-            Err(err) => eprintln!("Failed to execute '{}': {}", exe_name, err),
-        }
-    } else {
+    if let Some(_path) = exe_path {
+        let child = Command::new(cmd).args(args).spawn().map_err(|err| err.to_string());
+        child?.wait().map_err(|err| err.to_string())?;
+        Ok(())
+    }
+    else{
         eprintln!("{}: command not found", cmd);
+        Err(String::from("command not found"))
     }
 }
